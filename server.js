cc4eb371393b6d0924d4d62e892728963a95bffb
Brian Turchyn <brian@dj-bri-t.net>
2018-05-20 20:44:26 -0500

    [WIP] Swap callbacks for promises

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ var startTime;
  * Establish DB connectivity
  */
 db.doInConn(function(conn, args, callback) {
-  conn.query('select 1 from channels', function (error, results, fields) {
-    callback(error);
-  });
+  db.query(conn, 'select 1 from channels LIMIT 0, 1')
+    .then(result => console.log(result))
+    .then(callback)
+    .catch(result => callback(result));
 });
 
 /**
@@ -42,35 +43,28 @@ io
     socket.join(channel);
     console.log(sid + " connected to " + channel);
     db.doInConn(function(conn, args, callback) {
-      conn.query("insert into attendees (name, channel_id, sid) VALUES (?, (SELECT id FROM channels WHERE user_key = ?), ?)",
-        [name, channel, sid],
-        function(error, results, fields) {
-          if (!error) {
-            io.to(channel).emit('user-connect', name);
-            sendChannelList(conn, channel, callback);
-            getQueue(conn, channel, function(error, result) {
-              if (!error) {
-                socket.emit('queue', result);
-              }
-            });
-            /*
-            io.to(channel).emit('queue', queue);
-            if (conchHolder != null) {
-              socket.emit('conch-holder', conchHolder);
-            }
-            //*/
-          } else {
-            callback(error);
-          }
-        });
+      Promise.all([
+        db.query(conn,
+          "insert into attendees (name, channel_id, sid) VALUES (?, (SELECT id FROM channels WHERE user_key = ?), ?)",
+          [name, channel, sid])
+          .then(function(results) {
+            io.to(channel).emit('user-connect', results);
+          }),
+        getChannelList(conn, channel).then(sendChannelList),
+        getQueue([conn, channel]).then(broadcastQueue)
+      ]).then(function() {
+        callback();
+      });
     });
+
     socket.on('disconnect', function(data) {
       console.log(sid + " disconnected from " + channel);
       db.doInConn(function(conn, args, callback) {
         conn.query("DELETE FROM attendees WHERE sid = ?", [sid], function(error, results, fields) {
           if (!error) {
             io.to(channel).emit('user-disconnect', name);
-            sendChannelList(conn, channel, callback);
+            getChannelList(conn, channel)
+              .then(sendChannelList);
           }
         });
       });
@@ -92,26 +86,23 @@ io
     });
 
     socket.on('hand-up', function() {
+      console.log('hand up');
       db.doInConn(function(conn, args, callback) {
-        addToQueue(conn, sid, channel, function(error) {
-          if (!error) {
-            sendQueue(conn, channel, callback);
-          } else {
-            callback(error);
-          }
-        });
+        addToQueue(conn, sid, channel)
+          .then(getQueue)
+          .then(broadcastQueue)
+          .then(callback)
+          .catch(callback);
       });
     });
 
     socket.on('hand-down', function() {
       db.doInConn(function(conn, args, callback) {
-        removeFromQueue(conn, sid, channel, function(error) {
-          if (!error) {
-            sendQueue(conn, channel, callback);
-          } else {
-            callback(error);
-          }
-        });
+        removeFromQueue(conn, sid, channel)
+          .then(getQueue)
+          .then(broadcastQueue)
+          .then(callback)
+          .catch(callback);
       });
     });
 
@@ -133,65 +124,75 @@ io
 var passConch = function (socket, channel) {
   db.doInConn(function(conn, args, callback) {
     console.log('Deleting');
-    deleteCurrentConchHolder(conn, channel, function(error) {
-      if (!error) {
-        console.log('Setting new holder');
-        passConchToUser(conn, channel, function(error) {
-          if (!error) {
-            console.log('Getting');
-            getConchHolder(conn, channel, function(error, result) {
-              if (!error) {
-                console.log("Emitting");
-                console.log(result);
-                io.to(channel).emit('conch-holder', result);
-              }
-              sendQueue(conn, channel, callback);
-            });
-          } else {
-            callback(error);
-          }
-        });
-      } else {
-        callback(error);
-      }
+    Promise.all([
+      deleteCurrentConchHolder(conn, channel)
+        .then(passConchToUser)
+        .then(getConchHolder)
+        .then(function(result) {
+          io.to(channel).emit('conch-holder', result);
+        }).then(function() {
+          return getQueue([conn, channel]);
+        }).then(broadcastQueue)
+        .catch(function(error) {
+          callback(error);
+        })
+    ]).then(function() {
+      callback();
     });
-    conn.query("SELECT q.* FROM queue q INNER JOIN attendees a ON a.id = q.attendee_id INNER JOIN channels c ON c.id = a.channel_id AND c.user_key = ? ORDER BY q.id LIMIT 0, 1",
-      [channel],
-      function(error, results, fields) {
-      });
   });
 };
 
-function deleteCurrentConchHolder(conn, channel, callback) {
-  conn.query("DELETE FROM queue WHERE start_time IS NOT NULL AND attendee_id IN (SELECT a.id FROM attendees a INNER JOIN channels c ON c.id = a.channel_id AND c.user_key = ?)",
-    [channel],
-    function(error, results, fields) {
-      callback(error);
-    }
-  );
+function deleteCurrentConchHolder(conn, channel) {
+  return new Promise(function(resolve, reject) {
+    conn.query("DELETE FROM queue WHERE start_time IS NOT NULL AND attendee_id IN (SELECT a.id FROM attendees a INNER JOIN channels c ON c.id = a.channel_id AND c.user_key = ?)",
+      [channel],
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve([conn, channel]);
+        }
+      });
+  });
 }
 
-function passConchToUser(conn, channel, callback) {
-  conn.query("UPDATE queue q SET start_time = ? WHERE attendee_id IN (SELECT a.id FROM attendees a INNER JOIN channels c ON c.id = a.channel_id AND c.user_key = ?) ORDER BY q.id LIMIT 1",
-    [Math.floor(new Date().getTime() / 1000), channel],
-    function(error, results, fields) {
-      callback(error);
-    }
-  );
-}
-
-function getConchHolder(conn, channel, callback) {
-  conn.query("SELECT q.*, a.sid FROM queue q INNER JOIN attendees a ON a.id = q.attendee_id INNER JOIN channels c ON c.id = a.channel_id WHERE c.user_key = ? AND q.start_time IS NOT NULL ORDER BY q.id ASC LIMIT 0, 1",
-    [channel],
-    function(error, results, fields) {
-      var result = [null, 0];
-      if (!error && results.length) {
-        console.log(results);
-        result = [results[0]['sid'], results[0]['start_time']];
+function passConchToUser(args) {
+  var conn = args[0],
+      channel = args[1];
+  return new Promise(function(resolve, reject) {
+    conn.query("UPDATE queue q SET start_time = ? WHERE attendee_id IN (SELECT a.id FROM attendees a INNER JOIN channels c ON c.id = a.channel_id AND c.user_key = ?) ORDER BY q.id LIMIT 1",
+      [Math.floor(new Date().getTime() / 1000), channel],
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve([conn, channel]);
+        }
       }
-      callback(error, result);
-    }
-  );
+    );
+  });
+}
+
+function getConchHolder(args) {
+  var conn = args[0],
+      channel = args[1];
+  return new Promise(function(resolve, reject) {
+    conn.query("SELECT q.*, a.sid FROM queue q INNER JOIN attendees a ON a.id = q.attendee_id INNER JOIN channels c ON c.id = a.channel_id WHERE c.user_key = ? AND q.start_time IS NOT NULL ORDER BY q.id ASC LIMIT 0, 1",
+      [channel],
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          var result = [null, 0];
+          if (!error && results.length) {
+            console.log(results);
+            result = [results[0]['sid'], results[0]['start_time']];
+          }
+          resolve(result);
+        }
+      }
+    );
+  });
 }
 
 /**
@@ -254,52 +255,89 @@ function onListening() {
   debug('Listening on ' + bind);
 }
 
-function sendChannelList(conn, channel, callback) {
-  conn.query("SELECT a.name, a.sid FROM channels c INNER JOIN attendees a ON a.channel_id = c.id WHERE c.user_key = ? ORDER BY name ASC",
-    [channel],
-    function(error, results, fields) {
-      var clients = {};
-      results.forEach(function(result) {
-        clients[result['sid']] = {name: result['name']};
-      });
-      io.to(channel).emit('clients', clients);
-      callback(error);
-    }
-  );
-}
-
-function addToQueue(conn, sid, channel, callback) {
-  conn.query("INSERT INTO queue (attendee_id) (select a.id FROM attendees a WHERE a.sid = ? AND NOT EXISTS (select 1 FROM queue q WHERE a.id = q.attendee_id));",
-    [sid],
-    function(error, results, fields) {
-      callback(error);
-    }
-  );
-}
-
-function removeFromQueue(conn, sid, channel, callback) {
-  conn.query("DELETE FROM queue WHERE attendee_id = (select a.id FROM attendees a INNER JOIN channels c ON c.id = a.channel_id WHERE a.sid = ? AND c.user_key = ?);",
-    [sid, channel],
-    function(error, results, fields) {
-      callback(error);
-    }
-  );
-}
-
-function sendQueue(conn, channel, callback) {
-  getQueue(conn, channel, function(error, result) {
-    if (!error) {
-      io.to(channel).emit('queue', result);
-    }
-    callback(error);
+function getChannelList(conn, channel) {
+  return new Promise(function(resolve, reject) {
+    conn.query("SELECT a.name, a.sid FROM channels c INNER JOIN attendees a ON a.channel_id = c.id WHERE c.user_key = ? ORDER BY name ASC",
+      [channel],
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          var clients = {};
+          results.forEach(function(result) {
+            clients[result['sid']] = {name: result['name']};
+          });
+          resolve([channel, clients]);
+        }
+      }
+    );
   });
 }
 
-function getQueue(conn, channel, callback) {
-  conn.query("SELECT a.sid FROM channels c INNER JOIN attendees a ON a.channel_id = c.id INNER JOIN queue q ON q.attendee_id = a.id WHERE c.user_key = ? AND q.start_time IS NULL ORDER BY q.id ASC",
-    [channel],
-    function(error, results, fields) {
-      callback(error, results.map(function(sid) { return sid['sid'] }));
-    }
-  );
+function sendChannelList(args) {
+  var channel = args[0],
+      clients = args[1];
+  return new Promise(function(resolve, reject) {
+    io.to(channel).emit('clients', clients);
+    resolve()
+  });
+}
+
+function addToQueue(conn, sid, channel) {
+  console.log('addToQueue');
+  return new Promise(function(resolve, reject) {
+    conn.query("INSERT INTO queue (attendee_id) (select a.id FROM attendees a WHERE a.sid = ? AND NOT EXISTS (select 1 FROM queue q WHERE a.id = q.attendee_id));",
+      [sid],
+      function(error, results, fields) {
+        console.log('resolving');
+        if (error) {
+          reject(error);
+        } else {
+          resolve([conn, channel]);
+        }
+      }
+    );
+  });
+}
+
+function removeFromQueue(conn, sid, channel) {
+  return new Promise(function(resolve, reject) {
+    conn.query("DELETE FROM queue WHERE attendee_id = (select a.id FROM attendees a INNER JOIN channels c ON c.id = a.channel_id WHERE a.sid = ? AND c.user_key = ?);",
+      [sid, channel],
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve([conn, channel]);
+        }
+      }
+    );
+  });
+}
+
+function broadcastQueue(args) {
+  var channel = args[0],
+      queue = args[1];
+  console.log('broadcastQueue');
+  return new Promise(function(resolve, reject) {
+    io.to(channel).emit('queue', queue);
+    resolve();
+  });
+}
+
+function getQueue(args) {
+  var conn = args[0],
+      channel = args[1];
+  return new Promise(function(resolve, reject) {
+    conn.query("SELECT a.sid FROM channels c INNER JOIN attendees a ON a.channel_id = c.id INNER JOIN queue q ON q.attendee_id = a.id WHERE c.user_key = ? AND q.start_time IS NULL ORDER BY q.id ASC",
+      [channel],
+      function(error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve([channel, results.map(function(sid) { return sid['sid'] })]);
+        }
+      }
+    );
+  });
 }
